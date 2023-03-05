40418221cb1bfcbf19db36de3cb8e7c775ebc550
Christian <60179743+christian-2hu@users.noreply.github.com>
2023-03-05 00:42:49 -0300

    Check if push was called
    If push was called, it will extract a value using a loop, concatenate in a integer, and then break when it gets to a # character

--- src/parser.c
+++ src/parser.c
@@ -2,13 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 #include "stack.c"
+#include <ctype.h>
+
+unsigned concatenate(unsigned x, unsigned y) {
+    unsigned pow = 10;
+    while(y >= pow)
+        pow *= 10;
+    return x * pow + y;        
+}
 
 void parse(FILE *file) {
     char ch;
     char buffer[256];
+    Node *stack = new_node();
 
     while(fgets(buffer, 256, file) != NULL) {
-        printf("%s", buffer);
+        if(strncmp("push", buffer, 4) == 0) {
+            int pushed_value = 0;
+            for(int i = 0; i < (int)strlen(buffer)+1; i++) {
+                if(isdigit(buffer[i])) {
+                    printf("%c\n", buffer[i]);
+                    int casted_char = buffer[i] - '0';
+                    pushed_value = concatenate(pushed_value, casted_char);
+                }
+                if(buffer[i] == '#') {
+                    break;
+                }
+            }
+            printf("Pushed value is %d\n", pushed_value);
+            stack_push(&stack, pushed_value);
+        }
     }
 }
 
